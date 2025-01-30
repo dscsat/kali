7ebc886122e754e41daa8b1edcc908cfe283e339
Digital Satellites Communication <hany@dscsat.com>
2025-01-30 02:12:55 +0200

    Update Allow Insecure Repositories.sh

--- Allow Insecure Repositories.sh
+++ Allow Insecure Repositories.sh
@@ -6,3 +6,13 @@ apt-get --allow-unauthenticated upgrade
 
 # wget https://http.kali.org/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb --no-check-certificate
 # sudo apt install ./kali-archive-keyring_2022.1_all.deb
+
+# [Fix Broken dependancy]
+apt --fix-broken install
+# [Apt Update]
+apt get update
+# [Apt key update]
+apt-key adv –keyserver keyserver.ubuntu.com –recv-keys 3B4FE6ACC0B21F32 # if this fails (it did on the VPS) due to a shared library was missing
+# [Installing gnupg package]
+apt-get install gnupg
+apt-key adv
